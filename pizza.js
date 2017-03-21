0242b8233f03d2db5a14bdd1a58d99549803df8c
Unknown <cfranklin8@mail.csuchico.edu>
2017-03-20 20:37:35 -0700

    class work 3/20

--- pizza.js
+++ pizza.js
@@ -1,7 +1,23 @@
-window.onload = main;
+window.onload = showValues;
 
-function main()
+function showValues()
 {
-  console.log("Hello World!");
+  var veg = document.getElementById("checkVeggies");
+  //veg.defaultChecked = true;
+  var name = document.getElementById("orderName");
+  //name.value = "Kevin";
+  console.log("Finished running JavaScript");
 }
 
+function getCustomerName()
+{
+  var name = document.getElementById("orderName");
+  console.log("Order received by " + name.value);
+
+  var veg = document.getElementById("checkVeggies").checked;
+  var pep = document.getElementById("checkPepperoni").checked;
+  var sau = document.getElementById("checkSausage").checked;
+  if(veg == true) {
+    console.log("Veggie Pizza");
+  }
+}
